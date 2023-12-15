34c73a88bc3b5d98f9f3a8fa3fc55feebbb7063f
Gaurav Meena <patelgaurav7734@gmail.com>
2023-12-15 18:46:39 +0530

    create a middleware and function to handle possible error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,14 +1,20 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
  
-export const signup=async (req,res)=>{
+export const signup=async (req,res,next)=>{
     const{username,email,password}=req.body;
     const hashedPassword= bcryptjs.hashSync(password,10);
     const newUser=new User({username,email,password:hashedPassword}); 
     try {
         await  newUser.save();
+        res.status(201).json("User created succesfully!");
     } catch (error) {
-        res.status(500).json(error.message)
+        // res.status(500).json(error.message) 
+        //calls the middleware in index.js
+        next(error);
+
+        // when we want to create a error use this i.e if a error is not thrown but still its a error
+        // next(errorHandler(550,'error from the function'))
     }
-    res.status(201).json("User created succesfully!");
 }
--- api/index.js
+++ api/index.js
@@ -22,11 +22,13 @@ app.listen(3000,()=>{
 app.use("/api/user",userRouter);
 app.use("/api/auth",authRouter);
   
-// app.get('/test', (req, res) => {
-//     // res.send('Hello World')
-//     res.json(
-//         {
-//             message:"Hello World"
-//         }
-//     )
-//  });
+//middleware for handling errors
+app.use((err,req,res,next)=>{
+    const statusCode=err.statusCode || 500;
+    const message=err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    })
+})
