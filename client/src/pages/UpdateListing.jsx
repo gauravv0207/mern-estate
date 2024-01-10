9ffedea4bb601a61fb552b4fe33adf0251a4f019
Gaurav Meena <patelgaurav7734@gmail.com>
2024-01-10 12:58:45 +0530

    complete listing page

--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -308,7 +308,10 @@ export default function UpdateListing() {
               />
               <div className="flex flex-col items-center">
                 <p>Regular Price</p>
-                <span className="text-xs">($ / month)</span>
+                {/* <span className="text-xs">($ / month)</span> */}
+                {formData.type === "rent" && (
+                  <span className="text-xs">($ / month)</span>
+                )}
               </div>
             </div>
 
@@ -326,7 +329,10 @@ export default function UpdateListing() {
                 />
                 <div className="flex flex-col items-center">
                   <p>Discounted Price</p>
-                  <span className="text-xs">($ / month)</span>
+                  {/* <span className="text-xs">($ / month)</span> */}
+                  {formData.type === "rent" && (
+                    <span className="text-xs">($ / month)</span>
+                  )}
                 </div>
               </div>
             )}
